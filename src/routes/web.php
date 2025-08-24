68799cdced784ef30f88f1d85dcc1ce3c8720e5b
ducvippro202 <hd108060fps@gmail.com>
2025-08-24 19:38:26 +0700

    Them route

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register-form');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login-form');
